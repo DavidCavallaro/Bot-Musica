07a3cb3ebd7f14e1873922beaa156f3ff25a7e11
0da0vi7d <david.cavallaro@iislagrangia.it>
2021-05-03 13:48:29 +0200

    Recreation of the command structure

--- commands/stop.js
+++ commands/stop.js
@@ -2,20 +2,20 @@ const { MessageEmbed } = require("discord.js");
 const sendError = require("../util/error");
 
 module.exports = {
-    name: "stop",
+  name: "stop",
 	async run (client, message, args) {
         const channel = message.member.voice.channel
-        if (!channel)return sendError("You are not in a voice channel!", message.channel);
+        if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
         const serverQueue = message.client.queue.get(message.guild.id);
-        if (!serverQueue)return sendError("There is nothing playing!", message.channel);
-   	    if(!serverQueue.connection)return
-	    if(!serverQueue.connection.dispatcher)return
+        if (!serverQueue)return sendError("There is nothing playing that I could stop for you.", message.channel);
+        if(!serverQueue.connection)return
+        if(!serverQueue.connection.dispatcher)return
         try{
-        serverQueue.connection.dispatcher.end();
+            serverQueue.connection.dispatcher.end();
         } catch (error) {
             message.guild.me.voice.channel.leave();
             message.client.queue.delete(message.guild.id);
-        return sendError(`Error: ${error}`, message.channel);
+            return sendError(`The player has stopped and the queue has been cleared: ${error}`, message.channel);
         }
         message.client.queue.delete(message.guild.id);
         serverQueue.songs = [];
