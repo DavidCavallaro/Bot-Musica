f8a540f42cc9a2e3bd20cceb70911d472ed141a7
0da0vi7d <david.cavallaro@iislagrangia.it>
2021-05-04 23:11:55 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,7 @@ module.exports = {
     name: 'ping',
     async run(client, message, args) {
         message.delete()
-        message.channel.send('Ping...').then(sent => {})
+        message.channel.send('Ping...').then(sent => {sent.edit(`This is my ping: ${client.ws.ping}ms`).then(message => message.delete({timeout:5000}))
+    })
     }
 }
