24e1cf48a3f9a67bd7363d987a17cca3494184ae
0da0vi7d <david.cavallaro@iislagrangia.it>
2021-05-04 23:12:40 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,10 +3,11 @@ const Discord = require('discord.js')
 const usedCommand = new Set()
 
 module.exports = {
-    name: 'ping',
-    async run(client, message, args) {
-        message.delete()
-        message.channel.send('Ping...').then(sent => {sent.edit(`This is my ping: ${client.ws.ping}ms`).then(message => message.delete({timeout:5000}))
-    })
-    }
+	name: 'ping',
+	async run(client, message, args) {
+		message.delete()
+		message.channel.send('Ping...').then(sent => {
+			sent.edit(`This is my ping: ${client.ws.ping}ms`).then(message => message.delete({timeout:5000}));
+		});
+	}
 }
